0664748e9315ae99a676e48e42201de9ea38ae7b
tyson lind <tysonfloydlind@gmail.com>
2024-03-12 19:35:38 -0600

    completed C# API response capturing

---
+++ KanyeWest/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace KanyeWest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var kanyeURL = "https://api.kanye.rest/";
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine(kanyeQuote);
+            Console.WriteLine(kanyeResponse.ToString());
+        }
+    }
+}
